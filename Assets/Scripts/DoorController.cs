0ca15e1613a24c69c9ad35be7c826032ce65a463
NyarlatMkII <coleminer273@gmail.com>
2025-10-31 17:21:25 -0700

    Procedural Room Movement
    -Player now moves to the next room after touching the door of a previous one.
    -Rooms are set up to go in random order
    -Player cannot go back to previous rooms
    
    TODO:
    -Add transition between rooms
    -Stop the same room from generating twice (Once we have enough rooms)
    -Add a counter so the boss room will be generated at the right time

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private GameObject gameController;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameController = GameObject.FindWithTag("GameController");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        gameController.GetComponent<GenerationController>().roomDone = true;
+    }
+
+}
